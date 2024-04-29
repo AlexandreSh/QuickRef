7322bb60f7a8345885d68ea30820de751cc777da
AlexandreSh <113853602+AlexandreSh@users.noreply.github.com>
2024-04-29 06:30:01 -0400

    directions to setting up

--- .loginscript.sh
+++ .loginscript.sh
@@ -4,3 +4,8 @@ export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
 
 echo "DBUS_SESSION_BUS_ADDRESS set to: $DBUS_SESSION_BUS_ADDRESS"
 
+####
+#append this in ~/.profile of the user logging in with u2f
+#  #script ran at login, currently fixes dbus errors for snaps
+#  source /path/to/this/file/.loginscript.sh
+#
